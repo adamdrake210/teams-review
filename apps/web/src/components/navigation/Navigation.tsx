39a3383c354f4448aa7dc50a0c0d9a282b238dfd
Adam Drake <addrake@ebay.com>
2021-12-25 12:59:37 +0100

    fix

--- apps/web/src/components/navigation/Navigation.tsx
+++ apps/web/src/components/navigation/Navigation.tsx
@@ -104,12 +104,13 @@ export const Navigation = () => {
               ) : (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
+                  className="h-6 w-6 fill-sky-500"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
+                    className="fill-sky-500"
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
