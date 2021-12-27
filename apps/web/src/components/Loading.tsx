0f2229cc5a18b59aff311e4367553e2f7ab70d86
Adam Drake <adamgedrake@gmail.com>
2021-12-27 20:23:38 +0100

    feature: loader (#17)
    Co-authored-by: Adam Drake <addrake@ebay.com>

--- apps/web/src/components/Loading.tsx
+++ apps/web/src/components/Loading.tsx
@@ -19,8 +19,27 @@ export const Loading = ({
   if (isLoading) {
     return (
       <div className="flex flex-col w-full justify-center items-center">
-        {/* <CircularProgress size={25} color="primary" className={classes.loader} /> */}
-        <p className="text-xl font-extralight">
+        <svg
+          className="animate-spin -ml-1 mr-3 h-5 w-5 text-green-700"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+        <p className="text-xl font-extralight text-green-700">
           {loadingMessage || "Loading..."}
         </p>
       </div>
