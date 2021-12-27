535476ff47f90c7a3406f1c39fe8ba126a18f52f
Adam Drake <addrake@ebay.com>
2021-12-27 11:58:44 +0100

    cleanup

--- apps/web/src/pages/team-members/edit/[id].tsx
+++ apps/web/src/pages/team-members/edit/[id].tsx
@@ -6,7 +6,7 @@ import prisma from "@/lib/prisma";
 import { Heading1 } from "@/components/ui/typography/Heading1";
 import { Layout } from "@/layout/Layout";
 import { LOGIN } from "@/constants/routerConstants";
-import { TeamMemberForm } from "@/components/TeamMemberForm";
+import { TeamMemberForm } from "@/components/teamMember/TeamMemberForm";
 import { returnSingleParam } from "@/utils/getSingleParam";
 
 export const getServerSideProps: GetServerSideProps = async ({
