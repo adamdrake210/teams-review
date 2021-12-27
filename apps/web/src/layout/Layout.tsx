3f6f7395d77e0c78e59ad8d9d852b21fbe8cc874
Adam Drake <addrake@ebay.com>
2021-12-27 20:30:36 +0100

    feature: footer

--- apps/web/src/layout/Layout.tsx
+++ apps/web/src/layout/Layout.tsx
@@ -1,8 +1,9 @@
 import React, { ReactNode } from "react";
 import Head from "next/head";
 
-import { Navigation } from "../components/navigation/Navigation";
+import { Navigation } from "@/components/navigation/Navigation";
 import { COMPANY_NAME } from "@/constants/constants";
+import { Footer } from "@/components/Footer";
 
 type LayoutProps = {
   children: ReactNode;
@@ -21,6 +22,7 @@ export const Layout = ({ children, title }: LayoutProps) => {
       <main className="flex flex-col min-h-screen font-raleway p-4 mx-auto container">
         {children}
       </main>
+      <Footer />
     </div>
   );
 };
