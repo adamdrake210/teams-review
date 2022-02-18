853a830d0b0674553970bad8d049dbc09a6df2d2
Adam Drake <addrake@ebay.com>
2022-02-18 08:47:39 +0100

    chore: update loader

--- apps/web/src/components/Loading.tsx
+++ apps/web/src/components/Loading.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Box, CircularProgress, Typography } from "@mui/material";
+
 import { ErrorText } from "./ui/typography/ErrorText";
 
 type Props = {
@@ -18,41 +20,39 @@ export const Loading = ({
 }: Props) => {
   if (isLoading) {
     return (
-      <div className="flex flex-col w-full justify-center items-center">
-        <svg
-          className="animate-spin -ml-1 mr-3 h-5 w-5 text-green-700"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="opacity-25"
-            cx="12"
-            cy="12"
-            r="10"
-            stroke="currentColor"
-            strokeWidth="4"
-          ></circle>
-          <path
-            className="opacity-75"
-            fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-          ></path>
-        </svg>
-        <p className="text-xl font-extralight text-green-700">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <CircularProgress />
+        <Typography variant="subtitle1" sx={{ my: 2 }}>
           {loadingMessage || "Loading..."}
-        </p>
-      </div>
+        </Typography>
+      </Box>
     );
   }
 
   if (isError) {
     return (
-      <div className="flex flex-col w-full justify-center items-center">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <ErrorText>
           There was a problem loading this request - {error && error.message}
         </ErrorText>
-      </div>
+      </Box>
     );
   }
 
